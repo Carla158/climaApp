a42fe7f00390cced7520c3c5267569f308bb01e7
carla158 <>
2018-09-25 22:14:37 -0300

    mejora

--- src/log.js
+++ src/log.js
@@ -2,9 +2,9 @@
  * Manejo los mensajes que se muestran en pantalla.
  */
 module.exports = function log(message, err) {
-  if (err) {
-    // console.log(err);
-    return;
-  }
-  console.log(message);
+    if (err) {
+        // console.log(err);
+        return;
+    }
+    console.log(message);
 }
